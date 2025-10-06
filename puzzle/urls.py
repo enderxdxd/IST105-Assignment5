ca5d38c30028113ed19de0dba234df6a432b0e78
Henrique Parreira Costa <148904673+enderxdxd@users.noreply.github.com>
2025-10-06 14:47:53 -0700

    fix:urls

--- puzzle/urls.py
+++ puzzle/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import puzzle_view
+from django.contrib import admin
+from django.urls import path, include
+from django.http import HttpResponse
 
+def healthz(_request):
+    return HttpResponse('ok', content_type='text/plain', status=200)
 
 urlpatterns = [
-path('', puzzle_view, name='puzzle'),
+    path('admin/', admin.site.urls),
+    path('', include('puzzle.urls')),
+    path('healthz', healthz),  # rota do Load Balancer
 ]
